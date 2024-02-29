6bbc1169299cce189a0487e082b0617567197ac5
kader_dashing <kaderdashing67@gmail.com>
2024-02-29 21:19:49 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+// import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar";
+import { ChakraProvider } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Router>
+      <ChakraProvider>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/NotFound" element={<NotFound />} />
+        </Routes>
+      </ChakraProvider>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function About() {
+  return <div style={{ border: "1px solid red" }}>About</div>;
+}
